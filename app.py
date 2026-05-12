1ab7ac50f4c67f3222d2f39d2c49298e3de6246c
SURIYA PRAKASH R M <suriyaprakashrm25@gmail.com>
2026-05-12 14:27:48 +0530

    Update home route response message

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Demo Successful 🚀"
+    return "CI/CD Demo Successful 🚀
+    hello everyone"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

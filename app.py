de429a17b009596ebcf804fdce77bf3bc9749f7a
SURIYA PRAKASH R M <suriyaprakashrm25@gmail.com>
2026-05-12 14:30:46 +0530

    Fix return statement formatting in home function

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Demo Successful 🚀
-    hello everyone"
+    return "CI/CD Demo Successful 🚀 hello everyone"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
